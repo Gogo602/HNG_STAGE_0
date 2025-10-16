c265a103643230dec21946114df59d8f451f51c1
Gogo602 <gogomarosi61@gmail.com>
2025-10-17 01:24:23 +0300

    Refactor profile card following all requirements

--- main.js
+++ main.js
@@ -89,3 +89,25 @@ if (skillList) {
 
     console.log(skillItem)
 }
+
+
+
+// Function to update the current time in milliseconds
+function updateUTCTime() {
+    //  Get current time in milliseconds 
+    const currentTimeMs = Date.now();
+
+    //  Select the target html element 
+    const timeElement = document.querySelector('[data-testid="test-user-time"]');
+
+    //  Update the element's content
+    if (timeElement) {
+        timeElement.textContent = "Time in Miliseconds: " + currentTimeMs;
+    }
+}
+
+// function call
+updateUTCTime();
+
+// update every 100 milliseconds)
+setInterval(updateUTCTime, 100);
